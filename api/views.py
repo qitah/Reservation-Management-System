9979365fdf115b1adb386dc8c51ba8febeca85ee
Abdullah <abdullah.qitah@gmail.com>
2022-09-26 14:58:57 +0300

    Create reservation view

--- api/views.py
+++ api/views.py
@@ -17,7 +17,7 @@ from users.serializers import CreateUserSerializer
 from reservation.models import Reservation
 from reservation.serializers import *
 from reservation.services import *
-from reservations.settings import START_WORKING_TIME, END_WORKING_TIME
+
 
 #from users.models import User
 # Create your views here.
@@ -58,52 +58,23 @@ class ReaservationCreatView(generics.CreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
 
-        serializer.is_valid(raise_exception=True)
-        table = serializer.validated_data['table']
-        group_size = serializer.validated_data['group_size']
-        new_start_time = serializer.validated_data['start_time']
-        new_end_time = serializer.validated_data['end_time']
+        if serializer.is_valid(raise_exception=True): 
+            table_id = serializer.validated_data['table']
+            group_size = serializer.validated_data['group_size']
+            new_start_time = serializer.validated_data['start_time']
+            new_end_time = serializer.validated_data['end_time']
+
+            if table_fits_the_group(group_size, table_id.id):
+                if within_working_hours(new_start_time, new_end_time):
+                    if check_for_new_reservation_time(new_start_time, new_end_time, table_id):
+                        self.perform_create(serializer)
+                        headers = self.get_success_headers(serializer.data)
+                        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+                    return Response({"Time": serializer.data["start_time"] +" - "+ serializer.data["end_time"] ,"message": "There is another reservation in this time slot"})
+                return Response({'message':'Reservation time should be within working hours from 12:00 - 23:59 '})
+            return Response({'message':'table does\'t fit the group'})    
         
-        if table_fits_the_group(group_size,table.id):
-            pass
-
-        if START_WORKING_TIME <= new_start_time <= END_WORKING_TIME and START_WORKING_TIME <= new_end_time <= END_WORKING_TIME:
-            return Response({'message':'Reservation time should be within working hours from 12:00 - 23:59 '})
-        
-        tables = Reservation.objects.filter(table=table, date=date.today())
-        serializer = CraeteReservationSerializer(data=request.data)
-
-        flag = check_for_new_reservation_time(new_start_time, new_end_time, tables)
-        
-        if serializer.is_valid(): 
-            if flag:
-                self.perform_create(serializer)
-                headers = self.get_success_headers(serializer.data)
-                return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-        return Response({"Time": serializer.data["start_time"] +" - "+ serializer.data["end_time"] ,"message": "check reservation details and try again"})
 
 
 
-def check_for_new_reservation_time(new_start_time:datetime.time, new_end_time:datetime.time, reservations:Sequence[Reservation]) -> bool:
-    return "s"
-    range_of_new_time = DateTimeRange(new_start_time, new_end_time)
-    flag = True
-
-    for reservation in reservations:
-        reserved_end_time = reservation.end_time
-        reserved_stert_time = reservation.start_time
-        range_of_reserved_time = DateTimeRange(reserved_stert_time, reserved_end_time)
-
-        if new_start_time in range_of_reserved_time and new_start_time != reserved_end_time:
-            return False
-        if new_end_time in range_of_reserved_time and new_end_time != reserved_stert_time:
-            return False            
-        if  reserved_end_time in range_of_new_time and new_start_time != reserved_end_time:
-            flag = False
-            break
-        if  reserved_stert_time in range_of_new_time and new_end_time != reserved_stert_time:
-            flag = False
-            break
-
-    return flag
     
--- reservation/services.py
+++ reservation/services.py
@@ -1,9 +1,45 @@
-from reservation.models import Table
+from datetime import date, datetime
+from reservation.models import Reservation, Table
 from django.db.models import Q
+from reservations.settings import START_WORKING_TIME, END_WORKING_TIME
+from datetimerange import DateTimeRange
 
 def table_fits_the_group(group_zize:int, table_id:id) -> bool:
     try:
         table = Table.objects.get(id=table_id)
+        number_of_seats = table.number_of_seats
+        if  group_zize == number_of_seats or group_zize == number_of_seats + 1:
+            return True
     except Table.DoesNotExist as e:
         print(e)
-    return True
+    return False
+
+def within_working_hours(start_time:datetime.time , end_time:datetime.time) -> bool:
+    if START_WORKING_TIME <= start_time <= END_WORKING_TIME and START_WORKING_TIME <= end_time <= END_WORKING_TIME:
+        return True
+    return False
+
+
+def check_for_new_reservation_time(new_start_time:datetime.time, new_end_time:datetime.time,  table_id:id) -> bool:
+    Reservations = Reservation.objects.filter(table=table_id, date=date.today())
+    new_start_time = str(new_start_time)
+    new_end_time = str(new_end_time)
+    range_of_new_time = DateTimeRange(new_start_time, new_end_time)
+    flag = True
+
+    for reservation in Reservations:
+        reserved_end_time = str(reservation.end_time)
+        reserved_stert_time = str(reservation.start_time)
+        range_of_reserved_time = DateTimeRange(reserved_stert_time, reserved_end_time)
+        
+        if new_start_time in range_of_reserved_time and new_start_time != reserved_end_time:
+            return False
+        if new_end_time in range_of_reserved_time and new_end_time != reserved_stert_time:
+            return False            
+        if  reserved_end_time in range_of_new_time and new_start_time != reserved_end_time:
+            return False 
+        if  reserved_stert_time in range_of_new_time and new_end_time != reserved_stert_time:
+            return False 
+
+    return flag
+
